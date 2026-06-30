6e8996ea1c14b822aa22c0333fcfaec68005b564
Filip Flejsierowicz <s26047@pjwstk.edu.pl>
2026-06-30 21:00:34 +0200

    Initial ASP.NET Core MVC project

---
+++ MiniHelpdesk/Data/HelpdeskContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MiniHelpdesk.Data;
+
+public class HelpdeskContext : DbContext
+{
+    public HelpdeskContext(DbContextOptions<HelpdeskContext> options)
+        : base(options)
+    {
+    }
+}
