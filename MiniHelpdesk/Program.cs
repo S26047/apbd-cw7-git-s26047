9433727e8da18e82b6bf885a34945ead0784ed5e
Filip Flejsierowicz <s26047@pjwstk.edu.pl>
2026-06-30 21:58:08 +0200

    Add request timing middleware

--- MiniHelpdesk/Program.cs
+++ MiniHelpdesk/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MiniHelpdesk.Data;
+using MiniHelpdesk.Middleware;
 using MiniHelpdesk.Repositories;
 using MiniHelpdesk.Repositories.Interfaces;
 using MiniHelpdesk.Services;
@@ -9,25 +10,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<HelpdeskContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddScoped<ITicketRepository, TicketRepository>();
 builder.Services.AddScoped<ITicketCommentRepository, TicketCommentRepository>();
 builder.Services.AddScoped<ITicketService, TicketService>();
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseMiddleware<RequestTimingMiddleware>();
 
 app.UseRouting();
 
