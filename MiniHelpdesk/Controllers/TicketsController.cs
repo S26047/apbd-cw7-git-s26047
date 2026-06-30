27b9f8e224afafbe30884c8a4a31032f87986a7f
Filip Flejsierowicz <s26047@pjwstk.edu.pl>
2026-06-30 21:43:31 +0200

    Add tickets listing page

---
+++ MiniHelpdesk/Controllers/TicketsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniHelpdesk.Services.Interfaces;
+
+namespace MiniHelpdesk.Controllers;
+
+public class TicketsController : Controller
+{
+    private readonly ITicketService _ticketService;
+
+    public TicketsController(ITicketService ticketService)
+    {
+        _ticketService = ticketService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var tickets = await _ticketService.GetAllAsync();
+        return View(tickets);
+    }
+}
